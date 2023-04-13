1a017f0d352aadb0cf1a41903d1e871de997edfc
Hamza Muhammad <hs8403340@gmaill.com>
2023-04-13 18:07:04 +0200

    Create function to check who wins
    Use of the switch cases was opted in this scenario so that all possibilities of the game could be covered

--- index.js
+++ index.js
@@ -2,16 +2,51 @@
 let getUserChoice = () => {
     let userChoice, temp;
     do {
-        userChoice = prompt("Choose from rock,paper or scissors");
-        temp = userChoice.toLowerCase();
+        temp = prompt("Choose from rock,paper or scissors");
+        userChoice = temp.toLowerCase();
     }
-    while (temp !== 'rock' && temp !== 'paper' && temp !== 'scissors');
-    return userChoice;
-
+    while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors');
+    return userChoice
 }
+// Create a function , which gets a  random choice out of the three pre-defined choices of the game and assigns them to our computer
+
 const getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * 3);
     return choices[random];
 }
-console.log(getComputerChoice());
+
+let playRound = (playerChoice, computerChoice) => {
+    let win = "You win!";
+    let defeat = "You Lose!"
+    switch (computerChoice) {
+        case 'rock':
+            switch (playerChoice) {
+                case 'scissors':
+                    return `${defeat}, ${computerChoice} beats ${playerChoice}`
+                    break;
+                case 'paper':
+                    return `${win}, ${playerChoice} beats ${computerChoice}`
+
+            }
+            break;
+        case 'paper':
+            switch (playerChoice) {
+                case 'rock':
+                    return `${defeat}, ${computerChoice} beats ${playerChoice}`;
+                    break;
+                case 'scissors':
+                    return `${win}, ${playerChoice} beats ${computerChoice}`;
+            }
+            break;
+        case 'scissors':
+            switch (playerChoice) {
+                case 'rock':
+                    return `${win}, ${playerChoice} beats ${computerChoice}`;
+                    break;
+                case 'paper':
+                    return `${defeat}, ${computerChoice} beats ${playerChoice}`;
+            }
+            break;
+    }
+}
