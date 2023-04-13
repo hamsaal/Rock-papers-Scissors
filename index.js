5e9cba44e06704f488dbe055a9e1a86cdcd0a1dc
Hamza Muhammad <hs8403340@gmaill.com>
2023-04-13 17:32:21 +0200

    Correct while condition for user's input function
    'AND' was replaced in the condition instead of multiple "OR" so that the user could not  get pass,  unless he/she/they have entered the correct input

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// Create a function, which displays the prompt and unless the user enters the correct input, show it again and again
 let getUserChoice = () => {
     let userChoice, temp;
     do {
         userChoice = prompt("Choose from rock,paper or scissors");
         temp = userChoice.toLowerCase();
     }
-    while (temp !== 'rock' || temp !== 'paper' || temp !== 'scissors');
+    while (temp !== 'rock' && temp !== 'paper' && temp !== 'scissors');
     return userChoice;
 
 }
