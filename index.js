373dfb90000310893aee385d7165409873de6084
Hamza Muhammad <hs8403340@gmaill.com>
2023-04-16 14:42:18 +0200

    Add function to fix undefined console logs
    A function was added since the playround function was not able to handle the case of similar choices and showed undefined in that case, this function solved the problem

--- index.js
+++ index.js
@@ -15,41 +15,53 @@ const getComputerChoice = () => {
     const random = Math.floor(Math.random() * 3);
     return choices[random];
 }
+// Create a function, which checks if both the parties have got same results after choice picking
+function checkTie(a, b) {
+    if (a === b) {
+        return true;
+    }
+}
+
 // Create a function, which will compare the user's choice with the computer's choice and show the results to use
 let playRound = (playerChoice, computerChoice) => {
     let win = "You win!";
     let defeat = "You Lose!"
     const myChoice = playerChoice();
     const cpuChoice = computerChoice();
-    switch (cpuChoice) {
-        case 'rock':
-            switch (myChoice) {
-                case 'scissors':
-                    return `${defeat}, ${cpuChoice} beats ${myChoice}`
-                    break;
-                case 'paper':
-                    return `${win}, ${myChoice} beats ${cpuChoice}`
+    if (checkTie(myChoice, cpuChoice)) {
+        console.log(`Nobody won try again`);
+    }
+    else {
+        switch (cpuChoice) {
+            case 'rock':
+                switch (myChoice) {
+                    case 'scissors':
+                        return `${defeat}, ${cpuChoice} beats ${myChoice}`
+                        break;
+                    case 'paper':
+                        return `${win}, ${myChoice} beats ${cpuChoice}`
 
-            }
-            break;
-        case 'paper':
-            switch (myChoice) {
-                case 'rock':
-                    return `${defeat}, ${cpuChoice} beats ${myChoice}`;
-                    break;
-                case 'scissors':
-                    return `${win}, ${myChoice} beats ${cpuChoice}`;
-            }
-            break;
-        case 'scissors':
-            switch (myChoice) {
-                case 'rock':
-                    return `${win}, ${myChoice} beats ${cpuChoice}`;
-                    break;
-                case 'paper':
-                    return `${defeat}, ${cpuChoice} beats ${myChoice}`;
-            }
-            break;
+                }
+                break;
+            case 'paper':
+                switch (myChoice) {
+                    case 'rock':
+                        return `${defeat}, ${cpuChoice} beats ${myChoice}`;
+                        break;
+                    case 'scissors':
+                        return `${win}, ${myChoice} beats ${cpuChoice}`;
+                }
+                break;
+            case 'scissors':
+                switch (myChoice) {
+                    case 'rock':
+                        return `${win}, ${myChoice} beats ${cpuChoice}`;
+                        break;
+                    case 'paper':
+                        return `${defeat}, ${cpuChoice} beats ${myChoice}`;
+                }
+                break;
+        }
     }
 }
 const game = () => {
